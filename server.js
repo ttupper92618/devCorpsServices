c797b5703584098823c82b1e1b6e4e673384f665
ttupper92618 <thomastupper92618@gmail.com>
2020-12-17 21:28:07 -0600

    rollback

--- server.js
+++ server.js
@@ -1,27 +1,8 @@
 const express = require("express");
-const mysql = require("mysql");
-const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
 
-var app = express();
-var port = process.env.PORT || 3000;
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "",
-  password: "",
-  database: "devcorps",
-  multipleStatements: true,
-});
-
-con.connect((err) => {
-  if (!err) {
-    console.log("connected");
-  } else {
-    console.log("connection failed");
-  }
-});
-
-var routes = require("./api/routes");
+const routes = require("./api/routes");
 routes(app);
 app.listen(port, function () {
   console.log("Server started on port: " + port);
