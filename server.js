f184aac3262823ada539f746955ab03d0dfaca2b
ttupper92618 <thomastupper92618@gmail.com>
2020-12-18 14:11:18 -0600

    get courses

--- server.js
+++ server.js
@@ -18,8 +18,28 @@ db.connect((err) => {
 const app = express();
 const port = process.env.PORT || 3000;
 
-const routes = require("./api/routes");
-routes(app);
+app.get("/gettest", (req, res) => {
+  let sql = "SELECT * from test";
+  let query = db.query(sql, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    res.send(results);
+  });
+});
+
+app.get("/courses", (req, res) => {
+  let sql = "SELECT * from courses";
+  let query = db.query(sql, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    res.send(results);
+  });
+});
+
+//const routes = require("./api/routes");
+//routes(app);
 app.listen(port, function () {
   console.log("Server started on port: " + port);
 });
