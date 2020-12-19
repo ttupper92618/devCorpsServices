6beee284e0ee314d238ed51d81f08dfc30e07da2
ttupper92618 <thomastupper92618@gmail.com>
2020-12-19 13:05:10 -0600

    fixed port

--- server.js
+++ server.js
@@ -10,28 +10,24 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
+    res.send("failed db_connect: " + err);
     throw err;
   }
   console.log("MySQL connected");
 });
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.get("/gettest", (req, res) => {
-  let sql = "SELECT * from test";
-  let query = db.query(sql, (err, results) => {
-    if (err) {
-      throw err;
-    }
-    res.send(results);
-  });
+app.get("/about", (req, res) => {
+  res.send("devcorps microservices 1.0.0");
 });
 
 app.get("/courses", (req, res) => {
   let sql = "SELECT * from courses";
   let query = db.query(sql, (err, results) => {
     if (err) {
+      res.send("failed /courses db select: " + err);
       throw err;
     }
     res.send(results);
@@ -43,6 +39,7 @@ app.get("/course_sections", (req, res) => {
   let sql = "SELECT * from course_sections WHERE courseId = ?";
   let query = db.query(sql, courseId, (err, results) => {
     if (err) {
+      res.send("failed /course_sections db select: " + err);
       res.send("failed for courseId: " + courseId);
       throw err;
     }
@@ -52,6 +49,6 @@ app.get("/course_sections", (req, res) => {
 
 //const routes = require("./api/routes");
 //routes(app);
-app.listen(port, function () {
+app.listen(3000, function () {
   console.log("Server started on port: " + port);
 });
